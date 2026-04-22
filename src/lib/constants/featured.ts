b5985258815acc9ec770ff45c5dccb8b31867130
Jawe <prdsjawe@gmail.com>
2026-04-22 21:21:40 +0800

    fix: update featured project images

--- src/lib/constants/featured.ts
+++ src/lib/constants/featured.ts
@@ -2,37 +2,37 @@ export const FeaturedProjects = [
 	{
 		title: 'Puerto Galera Pass',
 		tags: ['Figma', 'Web Design', 'Travel Registration System'],
-		image: '/featured/pgpass.png',
+		image: '/featured/pgpass.webp',
 		link: '/projects/puerto-galera-pass'
 	},
 	{
 		title: 'Sprix UI',
 		tags: ['Figma', 'Web Design', 'Design System'],
-		image: '/featured/sprixui.png',
+		image: '/featured/sprixui.webp',
 		link: '/projects/sprix-ui'
 	},
 	{
 		title: 'Mivora',
 		tags: ['Figma', 'Web Design', 'Task Management'],
-		image: '/featured/mivora.png',
+		image: '/featured/mivora.webp',
 		link: '/projects/mivora'
 	},
 	{
 		title: 'Bookly',
 		tags: ['Figma', 'Web Design', 'Books'],
-		image: '/featured/bookly.png',
+		image: '/featured/bookly.webp',
 		link: '/projects/bookly'
 	},
 	{
 		title: 'Pop pop',
 		tags: ['Figma', 'Web Design', 'Popup Service'],
-		image: '/featured/poppop.png',
+		image: '/featured/poppop.webp',
 		link: '/projects/pop-pop'
 	},
 	{
 		title: 'Zapcab',
 		tags: ['Figma', 'Web Design', 'Car Hailing'],
-		image: '/featured/zapcab.png',
+		image: '/featured/zapcab.webp',
 		link: '/projects/zapcab'
 	}
 ];
